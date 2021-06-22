6f93c12ebb08e215bbc4ba119f56bb520f054126
Manpreet_singh <ms4110415@gmail.com>
2021-06-22 17:51:22 +0530

    version updated

--- _site/assets/js/advancedFilter2.js
+++
@@ -1,5 +0,0 @@
-const advancedFilter = document.querySelector('.advancedFilter')
-const fillterWrapper = document.querySelector('.fillterWrapper')
-advancedFilter.addEventListener('click', () => {
-  fillterWrapper.classList.toggle('active')
-})
